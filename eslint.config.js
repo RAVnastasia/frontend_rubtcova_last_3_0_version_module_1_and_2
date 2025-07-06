ccc995c3ac0b52c02f5a8df4620e466a173f85e4
Иван Хохолков <ivankhokholkov@MacBook-Air-Ivan.local>
2025-07-06 14:38:36 +0300

    Добавлен чек-лист контроля качества и настройки линтеров - Анастасия Рубцова СИ-25

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        fetch: "readonly",
+        Swiper: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "eqeqeq": "error",
+      "no-var": "error",
+      "prefer-const": "warn"
+    }
+  }
+];
